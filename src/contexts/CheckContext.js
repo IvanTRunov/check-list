03c8bc71acc0fb1fa1d76e5b404be5d3974f146d
IvanTRunov <58050235+IvanTRunov@users.noreply.github.com>
2020-07-01 20:02:04 +0300

    add LocalStorage

--- src/contexts/CheckContext.js
+++ src/contexts/CheckContext.js
@@ -1,12 +1,18 @@
-import React, { useReducer, createContext } from 'react'
-import {CheckReducer} from '../reducers/CheckReducer'
+import React, { useReducer, createContext, useEffect } from 'react'
+import { CheckReducer } from '../reducers/CheckReducer'
 
 export const CheckContext = createContext()
 
 const CheckContextProvider = (props) => {
-    const [checks, dispatch] = useReducer(CheckReducer, [])
+    const [checks, dispatch] = useReducer(CheckReducer, [], ()=>{
+        const LocalData = localStorage.getItem('checks');
+        return LocalData ? JSON.parse(LocalData):[]
+    });
+    useEffect(() => {
+        localStorage.setItem('checks', JSON.stringify(checks))
+    }, [checks]);
     return (
-        <CheckContext.Provider value={{checks, dispatch}}>
+        <CheckContext.Provider value={{ checks, dispatch }}>
             {props.children}
         </CheckContext.Provider>
     )
